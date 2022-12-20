989c47eb8c590d6c2a7eba1f89058db965eb4503
Kiran2821996 <kirandchennai@gmail.com>
2022-12-20 11:28:03 +0530

    longshot

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -3,7 +3,16 @@ import React, { useState, createContext } from "react";
 const fecthedData = createContext();
 
 function Context(props) {
-  const [mapData, setMapData] = useState([]);
+  const [mapData, setMapData] = useState([
+    "guide",
+    "33100",
+    "0",
+    "0.64",
+    "0.01",
+    "14720000000",
+    "0.27,0.27,0.27,0.27,0.27,0.27,0.18,0.18,0.45,1.00,0.27,0.27",
+    "11"
+  ]);
 
   return (
     <fecthedData.Provider value={{ mapData, setMapData }}>
